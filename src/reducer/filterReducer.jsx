08e73262b89c707708ec868abcbc3c9cf805c2de
oliullahlimon <oliullah.limon.55@gmail.com>
2025-07-18 18:23:13 +0600

    updated

--- src/reducer/filterReducer.jsx
+++ src/reducer/filterReducer.jsx
@@ -2,10 +2,6 @@ const filterReducer = (state, action) => {
   switch (action.type) {
     case "LOAD_FILTER_PRODUCTS":
       let priceArr = action.payload.map((curElem) => curElem.price);
-      console.log(
-        "🚀 ~ file: filterReducer.js ~ line 5 ~ filterReducer ~ priceArr",
-        priceArr
-      );
 
       // 1way
       // console.log(Math.max.apply(null, priceArr));
@@ -20,10 +16,6 @@ const filterReducer = (state, action) => {
       // );
 
       let maxPrice = Math.max(...priceArr);
-      console.log(
-        "🚀 ~ file: filterReducer.js ~ line 23 ~ filterReducer ~ maxPrice",
-        maxPrice
-      );
 
       return {
         ...state,
